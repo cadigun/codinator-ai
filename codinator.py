0dda00c2b1b349336a56d07b8843948ef4bec08f
Christianah Adigun <chrissy-adigun@hotmail.com>
2024-03-08 14:07:20 -0500

    cleanup formatting

--- codinator.py
+++ codinator.py
@@ -10,25 +10,27 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
 
 def main():
     logging.debug("Running Codinator...")
-    filepath = os.environ.get("SPEC_FILE_PATH" or '.codinator-spec-sample')
-    if not filepath:
-        logging.error("No spec file found. Exiting...")
+    filepath = os.environ.get("SPEC_FILE_PATH") or '.codinator-spec-sample'
+    spec = read_spec_from_yaml(filepath)
+    if not spec.requirements:
+        logging.debug("No requirements specified. Exiting...")
         return
 
-    spec = read_spec_from_yaml(filepath)
     requirements_text = "\n".join(f"- {req}" for req in spec.requirements)
     logging.debug(f'Code Review Requirements:\n{requirements_text}')
 
     changed_files = git_changed_files(spec.default_branch)
-    logging.debug(f'Changed Files: {changed_files}')
+    # print the changed files array into line by line
 
     files_to_be_checked = filter_files_by_type(changed_files, spec.files_types)
     if not files_to_be_checked:
         logging.debug("No files to be checked. Exiting...")
         return
 
+    logging.debug(f"Files to be reviewed:'\n'.join(files_to_be_checked)")
+
     for file in files_to_be_checked:
-        print(f"**### Code Review Comments for {file}**:")
+        print(f"** ------------- {file} ------------- **:")
         file_diff = git_diff(spec.default_branch, file)
         print(f"{get_openai_response(requirements_text, file_diff)}\n\n")
 
